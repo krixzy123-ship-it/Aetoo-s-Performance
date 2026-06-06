bcd85bf7167566e5136addd601998117edf43e67
krixzy123-ship-it <krixzy123@gmail.com>
2026-06-07 01:41:42 +0200

    Update bot.js

--- src/config/bot.js
+++ src/config/bot.js
@@ -25,7 +25,7 @@ export const botConfig = {
     activities: [
       {
         // Text users will see (example: "Playing /help | Titan Bot").
-        name: "Orangeje Stad.",
+        name: "Aetoo's Performance",
         // Activity type number (0 = Playing).
         type: 0, 
       },
