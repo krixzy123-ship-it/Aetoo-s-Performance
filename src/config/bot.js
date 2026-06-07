124b254f9ea6bbe45c02e6fb2cba6ccd4713f9e0
krixzy123-ship-it <krixzy123@gmail.com>
2026-06-07 02:18:39 +0200

    Update bot.js

--- src/config/bot.js
+++ src/config/bot.js
@@ -104,7 +104,7 @@ export const botConfig = {
 
       // Discord-style palette shortcuts.
       blurple: "#5865F2",
-      green: "#57F287",
+      red: "#ff0000",
       yellow: "#FEE75C",
       fuchsia: "#EB459E",
       red: "#ED4245",
